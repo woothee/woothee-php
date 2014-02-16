dc3a7981eb9b577fa156303548bfed1826e3fa73
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-02-16 14:05:52 +0900

    Replace double-quotes with single-quotes

--- src/Woothee/AgentCategory/Appliance/Nintendo.php
+++ src/Woothee/AgentCategory/Appliance/Nintendo.php
@@ -10,14 +10,14 @@ class Nintendo extends AbstractCategory
     {
         $data = null;
 
-        if (strpos($ua, "Nintendo 3DS;") !== false) {
-          $data = DataSet::get("Nintendo3DS");
-        } elseif (strpos($ua, "Nintendo DSi;") !== false) {
-          $data = DataSet::get("NintendoDSi");
-        } elseif (strpos($ua, "Nintendo Wii;") !== false) {
-          $data = DataSet::get("NintendoWii");
-        } elseif (strpos($ua, "(Nintendo WiiU)") !== false) {
-          $data = DataSet::get("NintendoWiiU");
+        if (strpos($ua, 'Nintendo 3DS;') !== false) {
+          $data = DataSet::get('Nintendo3DS');
+        } elseif (strpos($ua, 'Nintendo DSi;') !== false) {
+          $data = DataSet::get('NintendoDSi');
+        } elseif (strpos($ua, 'Nintendo Wii;') !== false) {
+          $data = DataSet::get('NintendoWii');
+        } elseif (strpos($ua, '(Nintendo WiiU)') !== false) {
+          $data = DataSet::get('NintendoWiiU');
         }
 
         if (is_null($data)) {
